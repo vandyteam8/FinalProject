b484a3fadc99f42894120baf86607561f737ee84
Mahesh Gouru <mgouru@my.loyno.edu>
2020-12-10 18:07:35 -0500

    Update

--- Master.py
+++ Master.py
@@ -3,6 +3,7 @@ try:
     import sys
     import datetime
     import time
+    import csv
     import boto3
     import threading
     print("All Modules Loaded ...... ")
@@ -68,13 +69,8 @@ class MyDb(object):
     @staticmethod
     def sensor_value():
 
-        pin = 23
-        sensor = Adafruit_DHT.DHT11
-
-        humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
-
-        if humidity is not None and temperature is not None:
-            print('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity))
+        if time is not None and BG is not None:
+            print('time={0:0.1f}*C  BG={1:0.1f}%'.format(temperature, humidity))
         else:
             print('Failed to get reading. Try again!')
         return Time, BG, CGM, CHO, insulin, LBGI, HBGI, Risk
